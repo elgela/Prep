87cd36bf731e7ea3c595b9d8dc2e974fd297c12c
Marcelo <gelatomarcelo@hotmail.com>
2021-10-21 01:21:43 -0300

    Completing

--- 04-JSIV/homework/homework.js
+++ 04-JSIV/homework/homework.js
@@ -50,8 +50,6 @@ function map(array, cb) {
 return array.map(cb);
 }
 
-
-
 function filter(array) {
   // Filtrar todos los elementos del array que comiencen con la letra "a".
   // Devolver un nuevo array con los elementos que cumplen la condición
